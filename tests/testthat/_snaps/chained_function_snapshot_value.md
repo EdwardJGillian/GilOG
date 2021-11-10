560d310f8b51fbd604c686f59ddc2a95f592336a
Edward Gillian <edwardjgillian@gmail.com>
2021-11-10 12:10:31 +0100

    test with snapshot value v1

---
+++ tests/testthat/_snaps/chained_function_snapshot_value.md
@@ -0,0 +1,50 @@
+# check file values for parameters
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["X", "organism", "kingdom", "taxid", "assembly"]
+        },
+        "class": {
+          "type": "character",
+          "attributes": {},
+          "value": ["data.frame"]
+        },
+        "row.names": {
+          "type": "integer",
+          "attributes": {},
+          "value": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        }
+      },
+      "value": [
+        {
+          "type": "integer",
+          "attributes": {},
+          "value": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        },
+        {
+          "type": "character",
+          "attributes": {},
+          "value": ["Lactobacillus sanfranciscensis", "Listeria marthii", "Leuconostoc carnosum", "Pediococcus ethanolidurans", "Enterococcus asini", "Pediococcus inopinatus", "Enterococcus rivorum", "Lactobacillus backii", "Enterococcus termitis", "Lactobacillus curieae", "Lacticaseibacillus paracasei", "Lactobacillus algidus", "Lactobacillus dextrinicus", "Oceanospirillum multiglobuliferum", "Lactobacillus selangorensis", "Listeria monocytogenes", "Lactobacillus curvatus", "Lactobacillus graminis", "Lactobacillus sakei", "Pediococcus stilesii"]
+        },
+        {
+          "type": "character",
+          "attributes": {},
+          "value": ["Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria", "Bacteria"]
+        },
+        {
+          "type": "integer",
+          "attributes": {},
+          "value": [1625, 529731, 1252, 319653, 57732, 114090, 762845, 375175, 332950, 1138822, 1597, 105612, 51664, 64969, 81857, 1639, 28038, 60519, 1599, 331679]
+        },
+        {
+          "type": "character",
+          "attributes": {},
+          "value": ["GCF_002907285.1", "GCF_014228045.1", "GCF_008690825.1", "GCF_007991575.1", "GCA_009917315.1", "GCF_001641385.1", "GCF_001742285.1", "GCF_002770655.1", "GCA_001886275.1", "GCF_000785105.2", "GCF_002406665.1", "GCF_007922695.1", "GCF_001436095.1", "GCF_002059185.1", "GCF_001436805.1", "GCF_003588085.1", "GCF_003410375.1", "GCF_001436415.1", "GCF_001435715.1", "GCF_005864415.1"]
+        }
+      ]
+    }
+
---
+++ tests/testthat/test-chained_function_snapshot_value.R
@@ -0,0 +1,110 @@
+library(GilOG)
+library(testthat)
+
+test_chained_functions <- function(csv_file1, csv_file2) {
+  testthat::local_edition(3)
+  test_that("check file values for parameters", {
+    # naming helper
+    tname <- function(n) {
+      paste0(home,
+             "/data/known_value/",
+             csv,
+             ".",
+             n,
+             ".test"
+      )
+    }
+
+    # create file path to csv file examples
+
+    csv_path1 <-
+      paste0(home, "/data/csv_examples/", csv_file1)
+    csv <- stringr::str_remove(csv_file1, ".csv")
+    df1 <- read.csv(file=csv_path1, na.strings = "")
+
+    csv_path2 <-
+      paste0(home, "/data/csv_examples/", csv_file2)
+    df2 <- read.csv(file=csv_path2, na.strings = "")
+
+    organism <- df1
+
+
+    testthat::expect_snapshot_value(
+      organism, style = "json2")
+
+    genes <- df2
+
+
+    testthat::expect_snapshot_value(
+      genes, style = "json2")
+
+    # dplyr needs to be run inside testthat for function to work
+    # generates a warning
+    suppressWarnings(library(dplyr))
+    summary_count_df <- GilOG::summary_count_processing(organism, genes)
+
+    testthat::expect_snapshot_value(
+      summary_count_df, style = "json2")
+
+    gpo_length_size <- GilOG::gene_length_size_calc(organism, genes)
+
+    testthat::expect_snapshot_value(
+      gpo_length_size, style = "json2")
+
+    co_ef_df <- GilOG::cor_processing(gpo_length_size)
+
+    testthat::expect_snapshot_value(
+      co_ef_df, style = "json2")
+
+    gpo_length_size_top_5 <- GilOG::cor_processing_top_5(gpo_length_size)
+
+    testthat::expect_snapshot_value(
+      gpo_length_size_top_5, style = "json2")
+
+    group_id <- as.double(1)
+    gpo_1 <- GilOG::gpo_output_processing(gpo_length_size_top_5, group_id)
+
+    testthat::expect_snapshot_value(
+      gpo_1, style = "json2")
+
+    group_id <- as.double(2)
+    gpo_2 <- GilOG::gpo_output_processing(gpo_length_size_top_5, group_id)
+
+    testthat::expect_snapshot_value(
+      gpo_2, style = "json2")
+
+    group_id <- as.double(3)
+    gpo_3 <- GilOG::gpo_output_processing(gpo_length_size_top_5, group_id)
+
+    testthat::expect_snapshot_value(
+      gpo_3, style = "json2")
+
+    group_id <- as.double(4)
+    gpo_4 <- GilOG::gpo_output_processing(gpo_length_size_top_5, group_id)
+
+    testthat::expect_snapshot_value(
+      gpo_4, style = "json2")
+
+    group_id <- as.double(5)
+    gpo_5 <- GilOG::gpo_output_processing(gpo_length_size_top_5, group_id)
+
+    testthat::expect_snapshot_value(
+      gpo_5, style = "json2")
+
+  })
+}
+
+# create the ALS file path
+home <- setwd(Sys.getenv("HOME"))
+
+
+csv_file_path <- file.path(getwd())
+csv_file_path <- file.path(home, "data/csv_examples")
+
+# create organism as test1.csv and genes as test2
+csv_files_list1 <- list.files(path = csv_file_path, pattern = "organisms*", full.names = FALSE)
+csv_files_list2 <- list.files(path = csv_file_path, pattern = "genes*", full.names = FALSE)
+
+# apply 1 list vector to the function
+# apply 2 list vector to function +
+purrr::map2(csv_files_list1, csv_files_list2, test_chained_functions)
