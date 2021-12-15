0a1ea0b9aab604b9ade744223cfc5dfa82fa9ae8
Edward Gillian <edwardjgillian@gmail.com>
2021-12-15 09:15:56 +0100

    updates after RCMD check

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+globalVariables(c(".","assembly", "gene_length", "gene_name", "gene_size",
+                  "group_id","location_end", "location_start",
+                  "organism","protein", "protein_id","r", "X")
+                )
